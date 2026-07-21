f2400b90572167cca39183a9ce9f1f139a83373c
clifforddonk <clifforddonk@.com>
2026-07-21 11:32:29 +0000

    new cahnges

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
